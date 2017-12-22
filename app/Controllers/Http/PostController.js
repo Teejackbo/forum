24fb1b45131248b0cbd171145dafd281a521c677
Jack Coldrick <jcldrk@gmail.com>
2017-12-22 15:38:35 +0000

    Create form and controller method for creating posts.

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,9 +1,12 @@
 'use strict'
 
 const Post = use('App/Models/Post')
+const Category = use('App/Models/Category')
 const { validate } = use('Validator')
+const { checkPerm } = use('App/Models/Helpers/UserHelper')
 
 class PostController {
+
   async index({ view }) {
     const posts = await Post.all()
     return view.render('posts.index', {
@@ -11,6 +14,16 @@ class PostController {
       posts: posts.toJSON()
     })
   }
+
+  async create({ view, response, auth }) {
+    checkPerm(auth.user.permissions, 1, response)
+    const categories = await Category.all()
+    return view.render('posts.create', {
+      title: 'Create a Post',
+      categories: categories.toJSON()
+    })
+  }
+
 }
 
 module.exports = PostController
