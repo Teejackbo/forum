93647a315e2bd401bc8bca9d6dd181b635b62e8e
Jack Coldrick <jcldrk@gmail.com>
2018-09-16 21:17:02 +0100

    Install cypress properly and write first tests.

---
+++ cypress/integration/pages.js
@@ -0,0 +1,25 @@
+describe('Pages', () => {
+  it('Home', () => {
+    cy.visit('')
+  })
+
+  it('Categories', () => {
+    cy.visit('/categories')
+
+    cy.get('.category')
+  })
+
+  it('Posts', () => {
+    cy.visit('/posts')
+
+    cy.get('.post')
+  })
+
+  it('Login', () => {
+    cy.visit('/login')
+  })
+
+  it('Register', () => {
+    cy.visit('/register')
+  })
+})
